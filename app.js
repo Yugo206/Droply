ec64c5d7e1f43b671e825f7dc19e682e7c709102
Yugo206 <218416914+Yugo206@users.noreply.github.com>
2026-07-05 15:46:15 +0200

    backend download

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const db = new Database("database.db");
 const { nanoid } = require("nanoid");
 const multer = require("multer");
 const path = require("path");
+const fs = require("fs");
+
+if (!fs.existsSync("uploads")) {
+    fs.mkdirSync("uploads");
+}
 
 const storage = multer.diskStorage({
     destination: "uploads/",
@@ -35,13 +40,45 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
 });
 
-// front-end API
 app.get("/f/:id", (req, res) => {
+    res.sendFile(__dirname + "/public/download.html");
+});
+
+// front-end API
+app.get("/api/file-info/:id", (req, res) => {
     const file = db.prepare("SELECT * FROM files WHERE id = ?").get(req.params.id);
+    
+    if (!file) {
+        return res.status(404).send("File not found.");
+    }
+    if (Date.now() > file.expires_at) {
+        return res.status(410).send("File has expired.");
+    }
+
+    res.json({
+        filename: file.filename,
+        size: file.size,
+        expiresAt: file.expires_at,
+        link: `/api/files/${file.id}`
+    });
+});
+
+app.get("/api/files/:id", (req, res) => {
+    const file = db.prepare("SELECT * FROM files WHERE id = ?").get(req.params.id);
+    
     if (!file) {
         return res.status(404).json({ error: "File not found." });
     }
-    req.sendFile(path.join(__dirname, "uploads", file.stored_name));
+
+    if (Date.now() > file.expires_at) {
+        return res.status(410).json({ error: "File has expired." });
+    }
+
+    res.download(path.join(__dirname, "uploads", file.stored_name), file.filename, (err) => {
+    if (err) {
+        return res.status(500).json({ error: "Error sending file." });
+    }
+});
 });
 
 app.post("/api/upload", upload.single("file"), (req, res) => {
